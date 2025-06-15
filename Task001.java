afc33c1f653b786ddc04469da69c8f5fea8432bc
dabdusam <samadaijaz26@gmail.com>
2025-06-15 14:11:22 +0530

    add Task001

---
+++ Task001.java
@@ -0,0 +1,7 @@
+public class Task001 {
+    public static void main(String[] args) {
+        System.out.println("Hello! My name is Samad.");
+        System.out.println("Today, I'm learning Java!");
+    }
+
+}
