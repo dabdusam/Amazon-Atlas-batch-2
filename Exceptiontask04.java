b754628d88f0b30cc2c9d9e6b2f4322d444ba78f
dabdusam <samadaijaz26@gmail.com>
2025-06-15 17:52:19 +0530

    Add files via upload

---
+++ Exceptiontask04.java
@@ -0,0 +1,18 @@
+package PracticeSet.atlaslearnings.day09;
+
+public class Exceptiontask04 {
+    public static void main(String[] args) {
+        try {
+
+            int[] myNumbers = {1, 2, 3};
+            System.out.println(myNumbers[2]);
+        }
+
+        catch (Exception e) {
+            System.out.println("Something went wrong.");
+        } finally {
+            System.out.println("I'm from finally block.");
+        }
+    }
+}
+
