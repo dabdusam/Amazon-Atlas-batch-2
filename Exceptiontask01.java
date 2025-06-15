cffcf6871466dd56e77bc840c486911dd736d2db
dabdusam <samadaijaz26@gmail.com>
2025-06-15 17:48:46 +0530

    Add files via upload

---
+++ Exceptiontask01.java
@@ -0,0 +1,15 @@
+package PracticeSet.atlaslearnings.day09;
+
+public class Exceptiontask01 {
+    public static void main(String[] args) {
+        int[] myNumbers = {1, 2, 3};
+        try{
+            System.out.println(myNumbers[10]);
+        } catch(Exception e){
+
+            System.out.println(e.getMessage());
+        }
+    }
+}
+
+
