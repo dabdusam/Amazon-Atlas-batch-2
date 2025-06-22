0b77ac8e6f06e72dcf94a4364068b652453c54d8
dabdusam <samadaijaz26@gmail.com>
2025-06-22 11:58:38 +0530

    Add files via upload

---
+++ Day11/Day11task12.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Day11task12 {
+    public static void main(String[] args) {
+        // Create an ArrayList of Strings
+        ArrayList<String> friends = new ArrayList<>();
+
+        // Add 5 friends' names to the list
+        friends.add("Himanshu");
+        friends.add("Hemant");
+        friends.add("Vaishakh");
+        friends.add("Rishab");
+        friends.add("Hassan");
+
+        // Print the list
+        System.out.println("My friends are: " + friends);
+    }
+}
+
