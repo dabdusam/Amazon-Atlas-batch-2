6e9de74c6032cd6944deb286eee0daf91f6da6e8
dabdusam <samadaijaz26@gmail.com>
2025-06-29 11:06:07 +0530

    Add files via upload

---
+++ Day13/Day13task07.java
@@ -0,0 +1,84 @@
+public class Day13task07 {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int value) {
+            this.data = value;
+            this.next = null;
+        }
+    }
+
+    static Node head = null;
+
+    public static void insertAtEnd(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    public static void deleteByValue(int value) {
+        if (head == null) return;
+
+        if (head.data == value) {
+            head = head.next;
+            return;
+        }
+
+        Node temp = head;
+        Node prev = null;
+
+        while (temp != null && temp.data != value) {
+            prev = temp;
+            temp = temp.next;
+        }
+
+        if (temp == null) return;
+
+        prev.next = temp.next;
+    }
+
+    public static void displayList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("NULL");
+    }
+
+    public static int getSize() {
+        int count = 0;
+        Node temp = head;
+        while (temp != null) {
+            count++;
+            temp = temp.next;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        insertAtEnd(19);
+        insertAtEnd(25);
+        insertAtEnd(42);
+
+        System.out.println("Original Linked List:");
+        displayList();
+        System.out.println("Size of the linked list: " + getSize());
+
+        deleteByValue(25);
+
+        System.out.println("After deleting 25:");
+        displayList();
+        System.out.println("Size of the linked list: " + getSize());
+    }
+}
+
