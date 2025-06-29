b96a3800a766bfa30f80af7221e802b4c2a029b5
dabdusam <samadaijaz26@gmail.com>
2025-06-29 11:05:48 +0530

    Add files via upload

---
+++ Day13/Day13task06.java
@@ -0,0 +1,74 @@
+public class Day13task06 {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int value) {
+            this.data = value;
+            this.next = null;
+        }
+    }
+
+    static Node head = null;
+
+    public static void insertAtEnd(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+        } else {
+            Node temp = head;
+            while (temp.next != null) {
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    public static void deleteByValue(int value) {
+        if (head == null) return;
+
+        if (head.data == value) {
+            head = head.next;
+            return;
+        }
+
+        Node temp = head;
+        Node prev = null;
+
+        while (temp != null && temp.data != value) {
+            prev = temp;
+            temp = temp.next;
+        }
+
+        if (temp == null) return;
+
+        prev.next = temp.next;
+    }
+
+    public static void displayList() {
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+
+        System.out.println("NULL");
+    }
+
+    public static void main(String[] args) {
+        insertAtEnd(19);
+        insertAtEnd(25);
+        insertAtEnd(42);
+
+        System.out.println("Original Linked List:");
+        displayList();
+
+        deleteByValue(25);
+
+        System.out.println("After deleting 25:");
+        displayList();
+    }
+}
+
