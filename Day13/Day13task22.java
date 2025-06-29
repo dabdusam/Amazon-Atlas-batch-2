589430d43b515834ca9d72bb313875c73e107866
dabdusam <samadaijaz26@gmail.com>
2025-06-29 11:11:52 +0530

    Add files via upload

---
+++ Day13/Day13task22.java
@@ -0,0 +1,19 @@
+import java.util.HashMap;
+
+public class Day13task22 {
+    public static void main(String[] args) {
+
+        // Creating a HashMap with initial capacity of 10
+        HashMap<String, Integer> hm2 = new HashMap<String, Integer>(10);
+
+        // Adding some key-value pairs
+        hm2.put("Apple", 1);
+        hm2.put("Banana", 2);
+        hm2.put("Cherry", 3);
+
+        // Printing the HashMap
+        System.out.println("HashMap with capacity 10: " + hm2);
+    }
+}
+
+
