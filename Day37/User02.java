99d37c1558b5f920a9ea30ff7bb95498ad398355
dabdusam <samadaijaz26@gmail.com>
2025-10-02 13:54:58 +0530

    User02code

---
+++ Day37/User02.java
@@ -0,0 +1,42 @@
+package org.example;
+
+import java.util.Date;
+
+public class User02 {
+    private Long id;
+    private String name;
+    private Date registrationDate;
+
+    // No-arg constructor
+    public User02() {}
+
+    // Constructor with id (for findById usage)
+    public User02(Long id) {
+        this.id = id;
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getRegistrationDate() {
+        return registrationDate;
+    }
+
+    public void setRegistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+}
